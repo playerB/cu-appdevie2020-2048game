40e8620870e678c4aa4de417d1eba34fd35fe8ad
playerB <skbrave1@gmail.com>
2020-11-18 11:55:06 +0700

    Add project files.

---
+++ 2048game/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace _2048game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scorelabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hscorelabel = new System.Windows.Forms.Label();
+            this.retrylabel = new System.Windows.Forms.Label();
+            this.gameoverlabel = new System.Windows.Forms.Label();
+            this.logolabel = new System.Windows.Forms.Label();
+            this.backlogo = new System.Windows.Forms.PictureBox();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.loadbutton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backlogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(285, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(66, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "current score";
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.AutoSize = true;
+            this.scorelabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.scorelabel.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorelabel.ForeColor = System.Drawing.Color.White;
+            this.scorelabel.Location = new System.Drawing.Point(66, 294);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(28, 31);
+            this.scorelabel.TabIndex = 1;
+            this.scorelabel.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(66, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "highest score";
+            // 
+            // hscorelabel
+            // 
+            this.hscorelabel.AutoSize = true;
+            this.hscorelabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.hscorelabel.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hscorelabel.ForeColor = System.Drawing.Color.White;
+            this.hscorelabel.Location = new System.Drawing.Point(66, 219);
+            this.hscorelabel.Name = "hscorelabel";
+            this.hscorelabel.Size = new System.Drawing.Size(28, 31);
+            this.hscorelabel.TabIndex = 1;
+            this.hscorelabel.Text = "0";
+            // 
+            // retrylabel
+            // 
+            this.retrylabel.AutoSize = true;
+            this.retrylabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.retrylabel.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.retrylabel.ForeColor = System.Drawing.Color.White;
+            this.retrylabel.Location = new System.Drawing.Point(549, 338);
+            this.retrylabel.Name = "retrylabel";
+            this.retrylabel.Size = new System.Drawing.Size(85, 35);
+            this.retrylabel.TabIndex = 2;
+            this.retrylabel.Text = "Retry";
+            this.retrylabel.Visible = false;
+            this.retrylabel.Click += new System.EventHandler(this.retrylabel_Click);
+            // 
+            // gameoverlabel
+            // 
+            this.gameoverlabel.AutoSize = true;
+            this.gameoverlabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.gameoverlabel.Font = new System.Drawing.Font("Microsoft Tai Le", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameoverlabel.ForeColor = System.Drawing.Color.White;
+            this.gameoverlabel.Location = new System.Drawing.Point(466, 263);
+            this.gameoverlabel.Name = "gameoverlabel";
+            this.gameoverlabel.Size = new System.Drawing.Size(253, 49);
+            this.gameoverlabel.TabIndex = 3;
+            this.gameoverlabel.Text = "GAME OVER!";
+            this.gameoverlabel.Visible = false;
+            // 
+            // logolabel
+            // 
+            this.logolabel.AutoSize = true;
+            this.logolabel.BackColor = System.Drawing.Color.Gold;
+            this.logolabel.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logolabel.ForeColor = System.Drawing.Color.White;
+            this.logolabel.Location = new System.Drawing.Point(84, 43);
+            this.logolabel.Name = "logolabel";
+            this.logolabel.Size = new System.Drawing.Size(114, 51);
+            this.logolabel.TabIndex = 4;
+            this.logolabel.Text = "2048";
+            // 
+            // backlogo
+            // 
+            this.backlogo.BackColor = System.Drawing.Color.Gold;
+            this.backlogo.Location = new System.Drawing.Point(61, 29);
+            this.backlogo.Name = "backlogo";
+            this.backlogo.Size = new System.Drawing.Size(188, 95);
+            this.backlogo.TabIndex = 5;
+            this.backlogo.TabStop = false;
+            // 
+            // savebutton
+            // 
+            this.savebutton.BackColor = System.Drawing.Color.SandyBrown;
+            this.savebutton.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savebutton.ForeColor = System.Drawing.Color.White;
+            this.savebutton.Location = new System.Drawing.Point(61, 135);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(91, 36);
+            this.savebutton.TabIndex = 6;
+            this.savebutton.Text = "save";
+            this.savebutton.UseVisualStyleBackColor = false;
+            this.savebutton.Click += new System.EventHandler(this.savebutton_Click);
+            // 
+            // loadbutton
+            // 
+            this.loadbutton.BackColor = System.Drawing.Color.SandyBrown;
+            this.loadbutton.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadbutton.ForeColor = System.Drawing.Color.White;
+            this.loadbutton.Location = new System.Drawing.Point(158, 135);
+            this.loadbutton.Name = "loadbutton";
+            this.loadbutton.Size = new System.Drawing.Size(91, 36);
+            this.loadbutton.TabIndex = 6;
+            this.loadbutton.Text = "load";
+            this.loadbutton.UseVisualStyleBackColor = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(906, 645);
+            this.Controls.Add(this.loadbutton);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.logolabel);
+            this.Controls.Add(this.gameoverlabel);
+            this.Controls.Add(this.retrylabel);
+            this.Controls.Add(this.hscorelabel);
+            this.Controls.Add(this.scorelabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.backlogo);
+            this.Name = "Form1";
+            this.Text = "2048";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.backlogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scorelabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label hscorelabel;
+        private System.Windows.Forms.Label retrylabel;
+        private System.Windows.Forms.Label gameoverlabel;
+        private System.Windows.Forms.Label logolabel;
+        private System.Windows.Forms.PictureBox backlogo;
+        private System.Windows.Forms.Button savebutton;
+        private System.Windows.Forms.Button loadbutton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ 2048game/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+2048 GAME
+Supawish Kanokpongsakorn 6230525521 CUIE103
+APP DEV FOR IE 2020
+ */
+
+namespace _2048game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rd = new Random();
+        int score = 0;
+        int row = 4;
+        int[,] board = new int[8, 8];
+        int dragx = 0, dragy = 0;
+        string direction = "";
+        int newsqx = 0, newsqy = 0;
+        bool isGameOver = false;
+
+        Bitmap t2 = new Bitmap("assets\\" + "tile2.png");
+        Bitmap t4 = new Bitmap("assets\\" + "tile4.png");
+        Bitmap t8 = new Bitmap("assets\\" + "tile8.png");
+        Bitmap t16 = new Bitmap("assets\\" + "tile16.png");
+        Bitmap t32 = new Bitmap("assets\\" + "tile32.png");
+        Bitmap t64 = new Bitmap("assets\\" + "tile64.png");
+        Bitmap t128 = new Bitmap("assets\\" + "tile128.png");
+        Bitmap t256 = new Bitmap("assets\\" + "tile256.png");
+        Bitmap t512 = new Bitmap("assets\\" + "tile512.png");
+        Bitmap t1024 = new Bitmap("assets\\" + "tile1024.png");
+        Bitmap t2048 = new Bitmap("assets\\" + "tile2048.png");
+
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            swipe(direction);
+            genTile();
+            scorelabel.Text = score.ToString();
+            Graphics g = e.Graphics;
+            int width = pictureBox1.Width / row;
+            int height = pictureBox1.Height / row;
+            Pen border_pen = new Pen(Color.FromArgb(127, 140, 141), 4.0f);
+
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < row; j++)
+                {
+                    
+                    switch (board[i, j])
+                    {
+                        case 2:
+                            g.DrawImage(t2, width * i, height * j, width, height);
+                            break;
+                        case 4:
+                            g.DrawImage(t4, width * i, height * j, width, height);
+                            break;
+                        case 8:
+                            g.DrawImage(t8, width * i, height * j, width, height);
+                            break;
+                        case 16:
+                            g.DrawImage(t16, width * i, height * j, width, height);
+                            break;
+                        case 32:
+                            g.DrawImage(t32, width * i, height * j, width, height);
+                            break;
+                        case 64:
+                            g.DrawImage(t64, width * i, height * j, width, height);
+                            break;
+                        case 128:
+                            g.DrawImage(t128, width * i, height * j, width, height);
+                            break;
+                        case 256:
+                            g.DrawImage(t256, width * i, height * j, width, height);
+                            break;
+                        case 512:
+                            g.DrawImage(t512, width * i, height * j, width, height);
+                            break;
+                        case 1024:
+                            g.DrawImage(t1024, width * i, height * j, width, height);
+                            break;
+                        case 2048:
+                            g.DrawImage(t2048, width * i, height * j, width, height);
+                            break;
+                        default:
+                            g.FillRectangle(Brushes.LightGray, width * i, height * j, width, height);
+                            break;
+                    }
+                }
+            for (int k = 0; k < row; k++)
+            {
+                g.DrawLine(border_pen, 0, height * k, pictureBox1.Width, height * k);
+                g.DrawLine(border_pen, width * k, 0, width * k, pictureBox1.Height);
+            }
+            g.DrawRectangle(Pens.Aqua, newsqx * width, newsqy * height, width, height);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragx = e.X;
+            dragy = e.Y;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (dragx > e.X && Math.Abs(dragx-e.X) > Math.Abs(dragy - e.Y))
+            {
+                direction = "left";
+            }
+            else if (dragx < e.X && Math.Abs(dragx - e.X) > Math.Abs(dragy - e.Y))
+            {
+                direction = "right";
+            }
+            else if (dragy > e.Y && Math.Abs(dragy - e.Y) > Math.Abs(dragx - e.X))
+            {
+                direction = "up";
+            }
+            else if (dragy < e.Y && Math.Abs(dragy - e.Y) > Math.Abs(dragx - e.X))
+            {
+                direction = "down";
+            }
+            else
+            {
+                direction = null;
+            }
+            Refresh();
+        }
+
+        void leftswipe(int row)
+        {
+            for(int t = 0; t < row; t++)
+            {
+                for (int j = 0; j < row; j++)
+                    for (int i = row - 2; i >= 0; i--)
+                    {
+                        for (int i_ = i; i_ >= 0; i_--)
+                        {
+                            if (board[i_, j] == 0)
+                            {
+                                board[i_, j] = board[i_ + 1, j];
+                                board[i_ + 1, j] = 0;
+                            }
+                        }
+                    }
+                for (int j = 0; j < row; j++)
+                    for (int i = 0; i < row - 1; i++)
+                    {
+                        if (board[i, j] == board[i + 1, j])
+                        {
+                            board[i, j] += board[i + 1, j];
+                            board[i + 1, j] = 0;
+                            score += board[i, j];
+                        }
+                    }
+            }
+        }
+
+        void rightswipe(int row)
+        {
+            for (int t = 0; t < row; t++)
+            {
+                for (int j = 0; j < row; j++)
+                    for (int i = 1; i < row; i++)
+                    {
+                        for (int i_ = i; i_ < row; i_++)
+                        {
+                            if (board[i_, j] == 0)
+                            {
+                                board[i_, j] = board[i_ - 1, j];
+                                board[i_ - 1, j] = 0;
+                            }
+                        }
+                    }
+                for (int j = 0; j < row; j++)
+                    for (int i = row - 1; i > 0; i--)
+                    {
+                        if (board[i, j] == board[i - 1, j])
+                        {
+                            board[i, j] += board[i - 1, j];
+                            board[i - 1, j] = 0;
+                            score += board[i, j];
+                        }
+                    }
+            }
+        }
+
+        void upswipe(int row)
+        {
+            for (int t = 0; t < row; t++)
+            {
+                for (int i = 0; i < row; i++)
+                    for (int j = row - 2; j >= 0; j--)
+                    {
+                        for (int j_ = j; j_ >= 0; j_--)
+                        {
+                            if (board[i, j_] == 0)
+                            {
+                                board[i, j_] = board[i, j_ + 1];
+                                board[i, j_ + 1] = 0;
+                            }
+                        }
+                    }
+                for (int i = 0; i < row; i++)
+                    for (int j = 0; j < row - 1; j++)
+                    {
+                        if (board[i, j] == board[i, j + 1])
+                        {
+                            board[i, j] += board[i, j + 1];
+                            board[i, j + 1] = 0;
+                            score += board[i, j];
+                        }
+                    }
+            }
+        }
+
+        void downswipe(int row)
+        {
+            for (int t = 0; t < row; t++)
+            {
+                for (int i = 0; i < row; i++)
+                    for (int j = 1; j < row; j++)
+                    {
+                        for (int j_ = j; j_ < row; j_++)
+                        {
+                            if (board[i, j_] == 0)
+                            {
+                                board[i, j_] = board[i, j_ - 1];
+                                board[i, j_ - 1] = 0;
+                            }
+                        }
+                    }
+                for (int i = 0; i < row; i++)
+                    for (int j = row - 1; j > 0; j--)
+                    {
+                        if (board[i, j] == board[i, j - 1])
+                        {
+                            board[i, j] += board[i, j - 1];
+                            board[i, j - 1] = 0;
+                            score += board[i, j];
+                        }
+                    }
+            }
+        }
+
+        private void retrylabel_Click(object sender, EventArgs e)
+        {
+            isGameOver = false;
+            score = 0;
+            board = new int[4, 4];
+            dragx = 0;
+            dragy = 0;
+            direction = "";
+            newsqx = 0;
+            newsqy = 0;
+            retrylabel.Visible = false;
+            gameoverlabel.Visible = false;
+            Refresh();
+        }
+
+        private void savebutton_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("save\\savegame1.txt");
+            for (int j = 0; j < row; j++)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    sw.Write(board[i, j].ToString() + ',');
+                }
+                sw.WriteLine("");
+            }
+            sw.Close();
+        }
+
+        public void swipe(string d)
+        {
+            if (d == null || isGameOver) return;
+            switch (d)
+            {
+                case "left":
+                    leftswipe(row);
+                    break;
+                case "right":
+                    rightswipe(row);
+                    break;
+                case "up":
+                    upswipe(row);
+                    break;
+                case "down":
+                    downswipe(row);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void genTile()
+        {
+            if (direction == null) return;
+            bool isFull = true;
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < row; j++)
+                {
+                    if (board[i, j] == 0) isFull = false;
+                }
+            if (isFull)
+            {
+                isGameOver = true;
+                retrylabel.Visible = true;
+                gameoverlabel.Visible = true;
+                return;
+            }
+
+            bool isPlaced = false;
+            do
+            {
+                int sqx = rd.Next(row);
+                int sqy = rd.Next(row);
+                if(board[sqx,sqy] == 0)
+                {
+                    board[sqx, sqy] = 2;
+                    isPlaced = true;
+                    newsqx = sqx;
+                    newsqy = sqy;
+                }
+            } while (!isPlaced);
+        }
+    }
+}
